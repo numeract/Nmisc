e59492616bca7bacce5f7fc56611c18ced829f34
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-02-13 14:34:12 +0200

    Added documentation for dplyr-misc

--- R/dplyr-misc.R
+++ R/dplyr-misc.R
@@ -1,7 +1,15 @@
 #' Pull out a single column using another column for names
 #'
-#' First verision, var must be character or numeric
-#'
+#' First version, var must be character or numeric
+#' 
+#' @param .data	 A table of data
+#' @param var 	A variable specified as: a literal variable name, 
+#' a positive integer, giving the position counting from the left, 
+#' a negative integer, giving the position counting from the right.
+#' @param name_col The name of the newly obtained column
+#' 
+#' @return the new table of data
+#' 
 #' @export
 pull_with_names <- function(.data, var = -1, name_col) {
     
