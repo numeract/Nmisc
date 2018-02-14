0960c06feab18816bc8c1552577f3413602cf02c
Teodor Ciuraru <teodorciuraru@Teos-Laptop.local>
2018-02-14 14:04:18 +0200

    Enhance documentation for set-misc.R

--- R/set-misc.R
+++ R/set-misc.R
@@ -1,13 +1,18 @@
-#' Performs Set Intersection
+#' Keep elements present in x and not contained in y
 #'
-#' Unlike intersect does not remove duplicates in x and keeps its order
+#' Unlike \code{\link[base]{base::intersect()}}, it does not remove duplicates in 
+#'   x and keeps its order.
+#' 
+#' @param x  Source vector.
+#' @param y  Destination vector (of the same mode as x).
 #'
-#' @param  x,y Vectors (of the same mode) containing a sequence of items
-#'
-#' @return A vector of the same mode as x or y
+#' @return A filtered version of x.
 #'
 #' @examples
-#' keep_if_in(1:5, 3:6)
+#' keep_if_in(1:5, 3:6) # [3, 4, 5]
+#' keep_if_in(c(4,3,4,3,1), 3:6) # [4 3 4 3]
+#'
+#' @seealso \code{\link{keep_if_not_in()}}
 #'
 #' @export
 keep_if_in <- function(x, y) {
@@ -15,16 +20,20 @@ keep_if_in <- function(x, y) {
     x[x %in% y]
 }
 
-#' Discards The Elements Present In A Set And Not Present In The Other
+#' Discard elements present in x and not contained in y
 #'
-#' Unlike setdiff does not remove duplicates in x and keeps its order
+#' Unlike \code{\link[base]{base::setdiff()}}, it does not remove duplicates in x 
+#'   and keeps its order.
 #'
-#' @param  x,y Vectors (of the same mode) containing a sequence of items
-#'
-#' @return A vector of the same mode as x or y
+#' @inheritParams keep_if_in
+#' 
+#' @return A filtered version of x.
 #'
 #' @examples
-#' keep_if_not_in(1:5, 3:6)
+#' keep_if_not_in(1:5, 3:6) # [1 2]
+#' keep_if_not_in(c(4,3,4,3,1), 3:6) # [1]
+#' 
+#' @seealso \code{\link{keep_if_in()}} 
 #'
 #' @export
 keep_if_not_in <- function(x, y) {
@@ -32,38 +41,33 @@ keep_if_not_in <- function(x, y) {
     x[!(x %in% y)]
 }
 
-
-#' Performs Set Intersection
-#'
-#' Unlike intersect does not remove duplicates in x and keeps its order
-#'
+#' @describeIn keep_if_in Shorthand version of \code{keep_if_in()}.
 #' @export
 `%if_in%` <- keep_if_in;
 
-
-#' Discards The Elements Present In A Set And Not Present In The Other
-#'
-#' Unlike setdiff does not remove duplicates in x and keeps its order
-#'
+#' @inherit keep_if_in
+#' @describeIn keep_if_not_in Shorthand version of \code{keep_if_not_in()}.
 #' @export
 `%if_not_in%` <- keep_if_not_in;
 
 
-#' Checks if two vectors are equal
+#' Check if two vectors are equal
 #' 
-#' Determines if two vectors have the same elements
+#' Wrapper over \code{\link{base::setequal()}} that adds extra parameter
+#'   \code{na.rm}.
 #' 
-#' @param x,y Vectors (of the same mode) containing a sequence of items
-#' @param na.rm logical, gives the posibility to choose
-#' whether or not to omit NA elements
+#' @param x,y Vectors (of the same mode) containing a sequence of items.
+#' @param na.rm Boolean value indicating whether \code{NA} should be ommited or not.
 #' 
-#' @return A logical scalar
+#' @return A logical scalar that states the result.
 #' 
-#' @example setequal_na(c(2, 1, 3), c(1, 2, 3))
-#' returns TRUE
+#' @examples
+#' setequal_na(c(2, 1, 3), c(1, 2, 3)) # TRUE
+#' setequal_na(c(1, NA, 3), c(3, NA, 1), na.rm = TRUE) # TRUE
+#' setequal_na(c(NA, NA), c(NA), na.rm = TRUE) # TRUE
+#' setequal_na(c(NA, NA), c(NA)) # TRUE
+#' setequal_na(c(1, 2, 3), c(1, 2, 3, NA)) # FALSE
 #' 
-#' @example setequal_na(c(1, NA, 3), c(3, NA, 1), na.rm = TRUE)
-#' returns TRUE
 #' @export
 setequal_na <- function(x, y, na.rm = FALSE) {
     
