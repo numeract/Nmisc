077dd60c8ed6dc3a01559e513e4382f6dfecdfce
Mike Badescu <mike.badescu@numeract.com>
2018-02-16 11:19:05 -0600

    added get_os()

---
+++ R/os.R
@@ -0,0 +1,8 @@
+#' Returns the name of the Operating Sytem
+#' 
+#' A simple wrapper arround \code{rappdirs:::get_os}, allowing it to be exported.
+#' 
+#' @return One of \code{"win"}, \code{"mac"}, \code{"unix"}, \code{"Unknown OS"}.
+#' 
+#' @export
+get_os <- getFromNamespace("get_os", "rappdirs")
