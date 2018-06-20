e291877cf5003cd68e0270764206899bca4e0c98
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-06-20 15:06:16 +0300

    Added code comments and refractor

--- R/project_packages.R
+++ R/project_packages.R
@@ -7,8 +7,8 @@ add_packages_info <- function(packages_df) {
         packages_df$package_name,
         function(x) x %in% INSTALLED_PACKAGES)
     for (i in 1:nrow(packages_df)) {
-        # if the package is already installed, we take information from package 
-        # descriptions, otherwise we look for it in CRAN repository
+        # if the package is already installed, take information from package 
+        # description, otherwise look for package in CRAN repository
         if (packages_df$is_installed[i]) {
             desc <-  utils::packageDescription(
                 packages_df$package_name[i], 
@@ -34,14 +34,18 @@ add_packages_info <- function(packages_df) {
 
 
 
-prepare_file_text <- function(include_pattern, exclude_pattern) {
+get_file_text <- function(include_pattern, exclude_pattern) {
     
+    # look in the files of interest only
     project_files <- list.files(
         path = ".", pattern = include_pattern, recursive = TRUE) %>%
         purrr::discard(~grepl(exclude_pattern, .))
     
+    # read every line of text from each file, discard commented lines,
+    # eliminate new line for Windows machines, eliminate spaces,
+    # eliminate new line for Unix/MaxOS, eliminate quotes, eliminate tabs
     exclude_comments_pattern <- '^#.*'
-    lines <- project_files %>%
+    lines <- project_files %>% 
         purrr::map(readLines) %>%
         unlist(use.names = FALSE) %>%
         purrr::discard(~grepl( exclude_comments_pattern, .)) %>%
@@ -52,14 +56,14 @@ prepare_file_text <- function(include_pattern, exclude_pattern) {
         stringr::str_replace_all('\'', '') %>%
         stringr::str_replace_all('\\t', '')
     
+    # create a string with all the pasted lines
     lines <- paste(lines, collapse = '')
-    
-    lines
 }
 
 
 get_loaded_packages <- function() {
     
+    # get the names of the packages already loaded in the current session
     packages <- dplyr::data_frame(
         package_name = names(sessionInfo()$loadedOnly), 
         stringsAsFactors = FALSE) %>%
@@ -71,9 +75,11 @@ get_loaded_packages <- function() {
 
 get_referenced_packages <- function(include_pattern, exclude_pattern) {
     
-    code_lines <- prepare_file_text(include_pattern, exclude_pattern)
+    code_lines <- get_file_text(include_pattern, exclude_pattern)
     
     regex_pattern <- '[[:alnum:]_.]+::'
+    # extract only the names of the packages used with "::" operator,
+    # add the column requested by, select rows with distinct package names
     packages <- stringr::str_extract_all(code_lines, regex_pattern) %>% 
         purrr::discard(~length(.) == 0) %>%
         unlist(use.names = FALSE) %>%
@@ -89,8 +95,9 @@ get_referenced_packages <- function(include_pattern, exclude_pattern) {
 
 get_library_packages <- function(include_pattern, exclude_pattern) {
     
-    code_lines <- prepare_file_text(include_pattern, exclude_pattern)
+    code_lines <- get_file_text(include_pattern, exclude_pattern)
     
+    # create a data frame with the packages loaded individually with "library"
     regex_pattern_single <- '(?<=library\\()([a-zA-Z1-9-_]+?)(?=\\))'
     single_packages <- stringr::str_extract_all(
         code_lines, regex_pattern_single) %>% 
@@ -98,6 +105,8 @@ get_library_packages <- function(include_pattern, exclude_pattern) {
         unlist(use.names = FALSE) %>% 
         dplyr::as_data_frame()
     
+    # create a data frame with multiple packages
+    # loaded with a single "library" call
     regex_pattern_multiple <- '(?<=library\\(c\\()(\\X+?)(?=\\)\\))'
     multiple_packages <- stringr::str_extract_all(
         code_lines, regex_pattern_multiple) %>%
@@ -107,25 +116,29 @@ get_library_packages <- function(include_pattern, exclude_pattern) {
         unlist(use.names = FALSE) %>%
         dplyr::as_data_frame()
     
-    packages <- single_packages %>%
+    # bind the two data frames 
+    all_library_packages <- single_packages %>%
         dplyr::bind_rows(multiple_packages)
     
-    if (nrow(packages) == 0) {
-        dplyr::as_data_frame(packages)
+    if (nrow(all_library_packages) == 0) {
+        dplyr::as_data_frame( all_library_packages)
     } else {
-        packages <- packages %>%
+        # rename columns, add requested_by column, select distinct rows taking 
+        # into consideration "package_name" column
+        all_library_packages <-  all_library_packages %>%
             dplyr::mutate(package_name = value,
                           requested_by  = "library") %>%
             dplyr::distinct(package_name, .keep_all = TRUE) 
-        add_packages_info(packages)
+        add_packages_info(all_library_packages)
     }
 }
 
 
 get_required_packages <- function(include_pattern, exclude_pattern) {
     
-    code_lines <- prepare_file_text(include_pattern, exclude_pattern)
+    code_lines <- get_file_text(include_pattern, exclude_pattern)
     
+    # create a data frame with the packages loaded individually with "require"
     regex_pattern_single <- '(?<=require\\()([a-zA-Z1-9-_]+?)(?=\\))'
     single_packages <- stringr::str_extract_all(
         code_lines, regex_pattern_single) %>% 
@@ -133,6 +146,8 @@ get_required_packages <- function(include_pattern, exclude_pattern) {
         unlist(use.names = FALSE) %>% 
         dplyr::as_data_frame()
     
+    # create a data frame with multiple packages
+    # loaded with a single "require" call
     regex_pattern_multiple <- '(?<=require\\(c\\()(\\X+?)(?=\\)\\))'
     multiple_packages <- stringr::str_extract_all(
         code_lines, regex_pattern_multiple) %>%
@@ -142,17 +157,20 @@ get_required_packages <- function(include_pattern, exclude_pattern) {
         unlist(use.names = FALSE) %>%
         dplyr::as_data_frame()
     
-    packages <- single_packages %>%
+    # bind the two data frames 
+    all_required_packages <- single_packages %>%
         dplyr::bind_rows(multiple_packages)
     
-    if (nrow(packages) == 0) {
-        dplyr::as_data_frame(packages) 
+    if (nrow(all_required_packages) == 0) {
+        dplyr::as_data_frame(all_required_packages) 
     } else {
-        packages <- packages %>%
+        # rename columns, add requested_by column, select distinct rows taking 
+        # into consideration "package_name" column
+        all_required_packages <- all_required_packages %>%
             dplyr::mutate(package_name = value,
                           requested_by = "require") %>%
             dplyr::distinct(package_name, .keep_all = TRUE)
-        add_packages_info(packages)
+        add_packages_info(all_required_packages)
     }
 }
 
